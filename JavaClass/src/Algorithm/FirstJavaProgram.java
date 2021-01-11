409c47c044d73d5bd2712ac0ce8bd152c20c3c52
ar_91 <ar_91@10.0.0.213>
2021-01-10 20:57:29 -0500

    first project

---
+++ JavaClass/src/Algorithm/FirstJavaProgram.java
@@ -0,0 +1,56 @@
+package Algorithm;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int variable1= 5;
+		
+		int Variable2= variable1  + 5;
+		int Variable3= Variable2 * 2;
+		int Variable4= Variable3 -7;
+		System.out.println(Variable4);
+		
+		
+//add three numbers 		
+			int num1 = 2;
+			int num2 = 3;
+			int num3 = 4;
+			
+			
+			int num4= num1 +num2 + num3;
+			System.out.println(num4);
+			
+	}
+	
+//calculate the average score
+	
+	{
+	int val1 = 1;
+	int val2 = 2;
+	int val3 = 3;
+	int val4 = 4;
+	int val5 = 5;
+	int val6 = 6;
+	int val7 = 7;
+	int val8 = 8;
+	int val9 = 9;
+	int val10 = 10;
+	int average = (val1 + val2 + val3 + val4 + val5 + val6 + val7 + val8 + val9 + val10)/10;
+	System.out.println(average);
+	
+	}
+
+
+}
+
+		
+		
+		
+		
+		
+	
+
+
